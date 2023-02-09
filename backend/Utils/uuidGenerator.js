048c7d88a616d740f0b301373af6167ac7385231
Charles Stello <58380022+SyntaxisCS@users.noreply.github.com>
2023-02-09 00:12:43 -0500

    BE 0.19.10
    Backend:
    - Added random bytes to end of email to ensure even repeat emails get a unique uuid

--- backend/Utils/uuidGenerator.js
+++ backend/Utils/uuidGenerator.js
@@ -1,6 +1,9 @@
 // UUID
 const uuid = require("uuid");
 
+// Crypto
+const crypto = require("crypto");
+
 // Dotenv
 const path = require("path");
 require("dotenv").config({path: path.resolve(__dirname, "../.env")});
@@ -8,7 +11,11 @@ require("dotenv").config({path: path.resolve(__dirname, "../.env")});
 const namespace = process.env.uuidNamespace;
 
 const generateUUID = (email) => {
-    return uuid.v5(email, namespace);
+    // Generate Random Characters to ensure even repeat emails get a new uuid
+    const randomBytes = crypto.randomBytes(4).toString("hex");
+    const randomizedEmail = email+randomBytes;
+
+    return uuid.v5(randomizedEmail, namespace);
 };
 
 module.exports = {generateUUID};
